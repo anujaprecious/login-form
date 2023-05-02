ad3bd4d873e876d22ad37597a7cc3c7925b94b5e
ANUJA <anuja.patel@preciousinfosystem.com>
2023-05-02 10:12:30 +0530

    Initial commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,4 +15,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();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
+
